91a93ce0961c2055c754f962a0ffcb0379888622
Alik Khilazhev <alikhil@mail.ru>
2016-12-09 23:28:52 +0300

    Adds minimal functional of web-server.
    * hello.html renamed to index.html
    * Shows list with xml files from ./samples directory in menu
    * Shows table of each by clicking on menu item
    * Adds custom styles.css for left navbar and highlighting cells.

--- webapp.py
+++ webapp.py
@@ -1,12 +1,32 @@
 from flask import Flask
 from flask import render_template
+from flask_bootstrap import Bootstrap
+from os import listdir
+from os.path import isfile, join
+from parser_xml import parse_standing_from_file as parse_file
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return render_template("hello.html", name="alik")
+    files = getFiles("./samples")
+    return render_template("index.html", files=files, standings=None)
+
+
+def getFiles(mypath):
+    return [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+
+@app.route("/showtable/<string:table>")
+def showtable(table):
+    files = getFiles("./samples")
+    standings = parse_file("./samples/" + table) \
+        if table in files else None
+
+    return render_template("index.html", files=files, standings=standings)
 
 
 if __name__ == "__main__":
-    app.run()
+    Bootstrap(app)
+    app.run(debug=True)
