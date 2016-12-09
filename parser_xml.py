0f40c402fb486ba24d06fe6fc3fe28bf1a401970
Alik Khilazhev <alikhil@mail.ru>
2016-12-09 23:26:07 +0300

    Renames parser.py to parser_xml.py.
    * Because there is exists system library with the same name.
    * Removes not needed code.

--- parser_xml.py
+++ parser_xml.py
@@ -5,7 +5,6 @@ from classes import Standings
 
 
 def parse_standing_from_file(filename):
-
     with open(filename, "r", encoding="utf8") as f:
         xmlData = f.read()
         print("xml read from " + filename)
@@ -14,7 +13,3 @@ def parse_standing_from_file(filename):
     obj = loads(json)
     standing = Standings(**obj["standings"])
     return standing
-
-
-standing = parse_standing_from_file("input.xml")
-print(standing.contest.sessions[0].submitted_problems[1].runs[0].accepted)
