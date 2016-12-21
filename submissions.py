18df264de7f737b552eafa1b175a0bfbc2d7ddd9
Alik Khilazhev <alikhil@mail.ru>
2016-12-22 01:17:48 +0500

    Updates submissions.py
    * Adds date filtering logic
    * Fixes unix_time

--- submissions.py
+++ submissions.py
@@ -1,8 +1,9 @@
 import config
+import time
 from datetime import datetime
 
 
-def get_submissions(totalStandings):
+def get_submissions(totalStandings, range_p):
     """Getting submissions from total standings"""
     submissions = list()
     for table in totalStandings.tables:
@@ -12,7 +13,20 @@ def get_submissions(totalStandings):
                     problem.name = table.contest.problem_alias[problem.alias]
                     submissions.append(Submission(
                         session.username, run, problem, table.contest.name))
-    return sorted(submissions, key=lambda s: s.unix_time, reverse=True)
+    sorted_ = sorted(submissions, key=lambda s: s.unix_time, reverse=True)
+    if range_p is None:
+        return sorted_
+    try:
+        dates = range_p.split("-")
+        start = time.mktime(
+            datetime.strptime(dates[0], "%d.%m.%Y").timetuple())
+        end = time.mktime(
+            datetime.strptime(dates[1], "%d.%m.%Y").timetuple()) + 86399
+        print(start, end)
+        return [sub for sub in sorted_
+                if sub.unix_time >= start and sub.unix_time <= end]
+    except:
+        return sorted_
 
 
 class Submission:
@@ -20,10 +34,10 @@ class Submission:
 
     def __init__(self, username, run, problem, contest_name):
         self.username = username
-        self.unix_time = run.time + config.CONTESTS_START_TIME
+        self.unix_time = (run.time + config.CONTESTS_START_TIME) / 1000
         self.problem_alias = problem.alias
         self.status = "success" if run.accepted == "yes" else ""
         self.problem_name = problem.name
         self.date = datetime.fromtimestamp(
-            self.unix_time / 1000).strftime("%d.%m.%Y %H:%M")
+            self.unix_time).strftime("%d.%m.%Y %H:%M")
         self.contest_name = contest_name
