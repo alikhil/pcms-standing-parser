dd5976a9e51b7793de392f94b105a7285815bf0c
Alik Khilazhev <alikhil@mail.ru>
2016-12-21 23:55:41 +0700

    Updates webapp.py
    * Adds /submissions handing

--- webapp.py
+++ webapp.py
@@ -1,10 +1,10 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from os import listdir
 from os.path import isfile, join
 from parser_xml import parse_standing_from_file as parse_file
 from classes import TotalStandings
+from submissions import get_submissions
 
 app = Flask(__name__)
 
@@ -34,6 +34,19 @@ def showtable(table):
         totalStandings=None)
 
 
+@app.route("/submissions", methods=["GET"])
+def show_submissions():
+    page_p = request.args.get("page")
+    page = 1 if page_p is None else int(page_p)
+
+    files = getFiles("./samples")
+    totalStandings = TotalStandings(
+        [parse_file("./samples/" + file) for file in files])
+    submissions = get_submissions(totalStandings)[(page - 1) * 50:50 * page]
+    return render_template(
+        "submissions.html", submissions=submissions, page=page)
+
+
 if __name__ == "__main__":
     Bootstrap(app)
-    app.run(host="0.0.0.0", port=1818)
+    app.run(host="0.0.0.0", port=1818, debug=True)
