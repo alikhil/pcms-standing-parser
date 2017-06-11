5347ddf8792824757da018806936ff839f32e64f
Alik Khilazhev <alikhil@mail.ru>
2017-06-11 19:38:34 +0300

    Adds fs_event_handler.py
    * Overriding FileSystemEventHandler

---
+++ fs_event_handler.py
@@ -0,0 +1,11 @@
+
+from watchdog.events import FileSystemEventHandler
+
+class FSEventHandler(FileSystemEventHandler):
+
+    def __init__(self, on_event):
+        self.on_event = on_event
+
+    def on_any_event(self, event):
+        print("some event happened: " + event.src_path + " " + event.event_type)
+        self.on_event()
